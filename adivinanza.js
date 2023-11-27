167dfa81eeb12c869cce888c0ab5fdda07cf0e1d
joseespejo <tinespejo14@gmail.com>
2023-11-27 14:41:42 -0300

    first commit

---
+++ adivinanza.js
@@ -0,0 +1,21 @@
+const generarNumeroAleatorio = () => {
+    return Math.floor(Math.random() * 100) + 1;
+};
+
+const verificarAdivinanza = (numeroSecreto, numeroAdivinado) => {
+
+    numeroAdivinado = parseInt(numeroAdivinado,10);
+
+    if (numeroAdivinado === numeroSecreto) {
+        console.log ('¡Felicitaciones! ¡Adivinaste el número secreto!');
+    } else if (numeroAdivinado > numeroSecreto) {
+        console.log ('El número secreto es menor. ¡Sigue intentando!');
+    } else {
+        console.log('El número secreto es mayor. ¡Sigue intentando!');
+    }
+};
+
+module.exports = {
+    generarNumeroAleatorio,
+    verificarAdivinanza
+}
